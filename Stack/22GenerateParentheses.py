456918c6ef4438de84241c5963734d2533cb9c7c
Yogesh Patil <officialyogeshp@gmail.com>
2023-12-25 15:46:32 -0500

    Added: 22 Generate Parentheses

--- Stack/22GenerateParentheses.py
+++ Stack/22GenerateParentheses.py
@@ -34,10 +34,29 @@ Best / Optimized Solution for review:
 
 
 # Test Cases
+# Set 01
+n = 3
+# Set 02
+n = 2
+
+def solution(n):
+  res = []
+
+  def backtrack(openN, closeN, path):
+    if openN == closeN == n:
+      res.append(path)
+      return 
+    
+    if openN < n:
+      backtrack(openN + 1, closeN, path + "(")
+
+    if closeN < openN:
+      backtrack(openN, closeN + 1, path + ")")
+
+  
+  backtrack(0,0, "")
+
+  return res
 
 
-def solution():
-  pass
-
-
-print(solution())
+print(solution(n))
