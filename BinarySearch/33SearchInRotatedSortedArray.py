a096d50b0acb8d2cbb4988dd0877344e02568287
Yogesh Patil <officialyogeshp@gmail.com>
2025-06-23 08:06:27 -0400

    UPDATED: #33 Search In Rotated Array -  (Spaced Repetition)

--- BinarySearch/33SearchInRotatedSortedArray.py
+++ BinarySearch/33SearchInRotatedSortedArray.py
@@ -51,6 +51,9 @@ Best / Optimized Solution for review:
 
 # Test Cases
 # Set 01
+from typing import List
+
+
 nums = [4,5,6,7,0,1,2]
 target = 0
 # Set 02
@@ -62,6 +65,35 @@ target = 0
 # Set 04
 nums = [1]
 target = 1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if nums[mid] == target: return mid
+
+            if nums[left] <= nums[mid]:
+              if target > nums[mid] or target < nums[left]:
+                  left = mid + 1
+              else:
+                  right = mid - 1
+            else:
+              if target < nums[mid] or target > nums[right]:
+                  right = mid - 1
+              else:
+                 left = mid + 1  
+        return -1
+
+
+
+
+
+
+
+
+
 
 def solution(nums, target):
   left, right = 0, len(nums) - 1
@@ -86,4 +118,7 @@ def solution(nums, target):
 
   return -1
 
-print(solution(nums, target))
+
+
+sol = Solution()
+print(sol.search(nums, target))
